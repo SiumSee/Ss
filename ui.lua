b407ce61d22621dc82aaa47cfbc28dac4c31d45a
SiumSee <93824340+SiumSee@users.noreply.github.com>
2021-11-21 10:38:03 +0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -14,11 +14,11 @@ local utility = {}
 local objects = {}
 local themes = {
 	Background = Color3.fromRGB(24, 24, 24), 
-	Glow = Color3.fromRGB(255, 255, 255), 
+	Glow = _G.Color, 
 	Accent = Color3.fromRGB(10, 10, 10), 
 	LightContrast = Color3.fromRGB(20, 20, 20), 
 	DarkContrast = Color3.fromRGB(14, 14, 14),  
-	TextColor = Color3.fromRGB(255, 255, 255)
+	TextColor = _G.Color
 }
 
 do
